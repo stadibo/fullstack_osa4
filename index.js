6faab5042f4907ed75ab85c6a434fde8f16f16f5
stadibo <jesperleonard@gmail.com>
2018-08-15 18:20:33 +0300

    4.1-2 done

--- index.js
+++ index.js
@@ -1,44 +1,33 @@
-//const http = require('http')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
 
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
-module.exports = Blog
+mongoose
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .then( () => {
+    console.log('connected to database', process.env.MONGODB_URI)
+  })
+  .catch( err => {
+    console.log(err)
+  })
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(middleware.logger)
 
-const mongoUrl = 'mongodb://peterzon:stack1@ds123012.mlab.com:23012/fullstack-blog'
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use(middleware.error)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
+const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ models/blog.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+const Blog = mongoose.model('Blog', {
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+module.exports = Blog
