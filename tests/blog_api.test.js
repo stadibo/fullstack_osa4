73e65f460ab8848033d5c613f004b4b616bc1c36
stadibo <jesperleonard@gmail.com>
2018-08-16 18:59:06 +0300

    4.10 noLikesTest created

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -2,6 +2,20 @@ const supertest = require('supertest')
 const { app, server } = require('../index')
 const api = supertest(app)
 
+const newBlog = {
+  title: 'Test are great, and here is why!',
+  author: 'Senior dev',
+  url: 'http://...',
+  likes: '0'
+}
+
+const noLikesBlog = {
+  title: 'Test are great, and here is why!',
+  author: 'Senior dev',
+  url: 'http://...',
+  likes: ''
+}
+
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
@@ -10,18 +24,17 @@ test('blogs are returned as json', async () => {
 })
 
 test('post should respond 201 if post succeeds', async () => {
-  const newBlog = {
-    title: 'Test are great, and here is why!',
-    author: 'Senior dev',
-    url: 'http://...',
-    likes: '0'
-  }
-
   await api
     .post('/api/blogs')
     .send(newBlog)
     .expect(201)
     .expect('Content-Type', /application\/json/)
+})
+
+test('post body with no likes value -> set likes: 0', async () => {
+  const response = await api.post('/api/blogs').send(noLikesBlog)
+
+  expect(response.body.likes).toBe(0)
 })
 
 afterAll(() => {
