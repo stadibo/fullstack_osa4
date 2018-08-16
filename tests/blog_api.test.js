80c4e221db9e39cc80cf83bf326645b3abe5f20a
stadibo <jesperleonard@gmail.com>
2018-08-16 18:14:14 +0300

    4.8 done

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -2,8 +2,8 @@ const supertest = require('supertest')
 const { app, server } = require('../index')
 const api = supertest(app)
 
-test('blogs are returned as json', () => {
-  return api
+test('blogs are returned as json', async () => {
+  await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
