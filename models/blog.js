b71cf683920b21195c89ced388176e617b2e8c94
stadibo <jesperleonard@gmail.com>
2018-08-17 17:59:11 +0300

    blog_api test refactoring

--- models/blog.js
+++ models/blog.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose')
-const Blog = mongoose.model('Blog', {
+const Schema = mongoose.Schema
+
+let blogSchema = new Schema({
   title: String,
   author: String,
   url: String,
   likes: Number
 })
+
+blogSchema.statics.format = (blog) => {
+  return {
+    title: blog.title,
+    author: blog.author,
+    url: blog.url,
+    likes: blog.likes,
+    id: blog._id
+  }
+}
+
+const Blog = mongoose.model('Blog', blogSchema)
 
 module.exports = Blog
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -2,57 +2,7 @@ const supertest = require('supertest')
 const { app, server } = require('../index')
 const api = supertest(app)
 const Blog = require('../models/blog')
-
-const initialBlogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: '5a422b891b54a676234d17fa',
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: '5a422ba71b54a676234d17fb',
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 0,
-    __v: 0
-  },
-  {
-    _id: '5a422bc61b54a676234d17fc',
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2,
-    __v: 0
-  }
-]
+const { initialBlogs, blogsInDb, nonExistingId } = require('./test_helper')
 
 const newBlog = {
   title: 'Test are great, and here is why!',
@@ -104,12 +54,12 @@ describe('blog tests', () => {
 
     expect(response.body.likes).toBe(0)
   })
-})
 
-test('creating blog without title or url gives bad request', async () => {
-  const response = await api.post('/api/blogs').send(titleLessBlog)
+  test('creating blog without title or url gives bad request', async () => {
+    const response = await api.post('/api/blogs').send(titleLessBlog)
 
-  expect(response.status).toBe(400)
+    expect(response.status).toBe(400)
+  })
 })
 
 afterAll(() => {
