095481c90e1cbf7d812bd2337f0e24e2a4ae2414
stadibo <jesperleonard@gmail.com>
2018-08-17 14:05:00 +0300

    4.11 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -10,15 +10,31 @@ blogsRouter.get('/', (request, response) => {
 })
 
 blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
-  if (!blog.likes) {
-    blog.likes = 0
-  }
+  const body = request.body
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
+  Blog
+    .find({ name: body.title })
+    .then(res => {
+      if (!body.title) {
+        return response.status(400).send({ error: 'no title' })
+      } else if (!body.url) {
+        return response.status(400).send({ error: 'no url' })
+      } else if (0 < res.length) {
+        return res.status(400).send({ error: 'blog already exists' })
+      } else {
+        if (!body.likes) {
+          console.log('no likes')
+          body.likes = 0
+        }
+
+        const blog = new Blog(body)
+        blog
+          .save()
+          .then(result => {
+            response.status(201).json(result)
+          })
+      }
+
     })
 })
 
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -58,7 +58,7 @@ const newBlog = {
   title: 'Test are great, and here is why!',
   author: 'Senior dev',
   url: 'http://...',
-  likes: '0'
+  likes: 0
 }
 
 const noLikesBlog = {
@@ -66,6 +66,11 @@ const noLikesBlog = {
   author: 'Senior dev',
   url: 'http://...',
   likes: ''
+}
+
+const titleLessBlog = {
+  author: 'Senior dev',
+  likes: 2
 }
 
 beforeAll(async () => {
@@ -78,25 +83,33 @@ beforeAll(async () => {
 
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('blog tests', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('post should respond 201 if post succeeds', async () => {
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('post body with no likes value -> set likes: 0', async () => {
+    const response = await api.post('/api/blogs').send(noLikesBlog)
+
+    expect(response.body.likes).toBe(0)
+  })
 })
 
-test('post should respond 201 if post succeeds', async () => {
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-})
+test('creating blog without title or url gives bad request', async () => {
+  const response = await api.post('/api/blogs').send(titleLessBlog)
 
-test('post body with no likes value -> set likes: 0', async () => {
-  const response = await api.post('/api/blogs').send(noLikesBlog)
-
-  expect(response.body.likes).toBe(0)
+  expect(response.status).toBe(400)
 })
 
 afterAll(() => {
